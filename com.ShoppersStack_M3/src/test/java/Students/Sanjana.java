3b59a69d500b0d804de44b94ababea7f10d53ac5
HP <HP@DESKTOP-QTCI61E>
2025-04-08 10:48:03 +0530

    New Commit By Sanjana

---
+++ com.ShoppersStack_M3/src/test/java/Students/Sanjana.java
@@ -0,0 +1,20 @@
+package Students;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sanjana {
+	
+	@Test
+	public void main()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.facebook.com");
+	}
+
+
